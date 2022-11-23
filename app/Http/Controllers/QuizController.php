37ae56110647b983e9b5755dc9f6431be48a1861
ymz-prog <ymz90288@gmail.com>
2022-11-24 00:19:37 +0900

    追加処理と削除処理のコントローラーを追加

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+
+
+        return view('quizzes.index');
+    }
+
+    public function destroy($id)
+    {
+        return json_encode(['message' => 'ID:'.$id.' が削除されるIDです(仮の出力)']);
+    }
+}
+
