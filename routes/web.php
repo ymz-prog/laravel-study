ed18cd0f1e0085fd173f3b16110d4172a2a3b2b2
ymz-prog <ymz90288@gmail.com>
2022-11-23 23:20:47 +0900

    テンプレートの実装

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/quizzes', function () {
+    return view('quizzes.index');
+});
+
+Route::get('quizzes/show', function () {
+    return view('quizzes.show');
+});
+
+Route::get('quizzes/create', function () {
+    return view('quizzes.create');
+});
+
+Route::delete('quizzes/{id}', function ($id) {
+    return json_encode(['message' => 'ID:'.$id.' が削除されるIDもです(仮の出力)']);
+});
