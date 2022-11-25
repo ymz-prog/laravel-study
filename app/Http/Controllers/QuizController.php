98465953f561fcc43ccffd634ebadfd10907f441
ymz-prog <ymz90288@gmail.com>
2022-11-26 02:11:22 +0900

    Modelの実装

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
+
+
 use Illuminate\Http\Request;
 
 class QuizController extends Controller
@@ -13,8 +16,9 @@ class QuizController extends Controller
      */
     public function index()
     {
-        //
-        return view('quizzes.index');
+        return view('quizzes.index', [
+            'quizzes' => Quiz::all(),
+        ]);
     }
 
     /**
@@ -38,7 +42,7 @@ class QuizController extends Controller
     {
         // 入力内容のチェック
         // ルールに一致しない入力の場合は、自動的に入力画面を表示させる
-        $request->validate([
+        $validatedData = $request->validate([
             'question' => 'required|max:255',
             'answer_a' => 'required|max:255',
             'answer_b' => 'required|max:255',
@@ -48,7 +52,22 @@ class QuizController extends Controller
             'explanation' => 'max:65535',
         ]);
 
-        return view('quizzes.index');
+        // Modelを作成
+        $Quiz = new Quiz;
+        $Quiz->question = $validatedData['question'];
+        $Quiz->answer_a = $validatedData['answer_a'];
+        $Quiz->answer_b = $validatedData['answer_b'];
+        $Quiz->answer_c = $validatedData['answer_c'];
+        $Quiz->answer_d = $validatedData['answer_d'];
+        $Quiz->correct_answer = $validatedData['correct_answer'];
+        $Quiz->explanation = $validatedData['explanation'];
+
+        // ModelをDBに保存
+        $Quiz->save();
+
+        // 一覧ページを表示
+        // ※ リロードされたときに、もう一度データが保存されないようにリダイレクトさせる
+        return redirect(route('quizzes.index'));
     }
 
     /**
@@ -59,8 +78,9 @@ class QuizController extends Controller
      */
     public function show($id)
     {
-        //
-        return view('quizzes.show');
+        return view('quizzes.show', [
+            'quiz' => Quiz::find($id),
+        ]);
     }
 
     /**
@@ -94,7 +114,14 @@ class QuizController extends Controller
      */
     public function destroy($id)
     {
-        //
-        return json_encode(['message' => 'ID:'.$id.' が削除されるIDです(仮の出力)']);
+        if (!Quiz::destroy($id)) {
+            // 400 Bad Request
+            return response()->json([
+                'message' => 'Failed to delete.',
+            ], 400);
+        }
+
+        // 204 NO CONTENT
+        return response()->noContent();
     }
 }
